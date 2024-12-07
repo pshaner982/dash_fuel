bdc7115eaa5c4e73eff8e8c31b2ce7d4d833a547
Patrick S <patrick.shaner@icloud.com>
2024-12-06 21:57:49 -0600

    fixed import errors

--- app/view_models/tanks/__init__.py
+++ app/view_models/tanks/__init__.py
@@ -1,4 +1,3 @@
 from .tanks import update_existing_tank, create_new_tank_for_company
 
-
-__all__ = [update_existing_tank, create_new_tank_for_company]
+__all__ = ["update_existing_tank", "create_new_tank_for_company"]
