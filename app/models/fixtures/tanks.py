b6fde1947bd0a7084820b9c724fb07607661f4ba
Patrick S <patrick.shaner@icloud.com>
2024-12-06 22:07:04 -0600

    created new fixture

--- app/models/fixtures/tanks.py
+++ app/models/fixtures/tanks.py
@@ -1,6 +1,6 @@
 import pytest
 from uuid import uuid4
-
+import random
 from app.models.tanks import Tanks
 from app.models.tanks_volumes import TankVolume
 
@@ -37,3 +37,16 @@ def mock_tank_volume():
         tank_id=uuid4(),
         volume=100,
     )
+
+@pytest.fixture
+@pytest.mark.django_db
+def sample_tank_volume(sample_tank, faker):
+    """Fixture to create and save a related company and tank instance to the test database."""
+    tank = sample_tank
+    tank = TankVolume.objects.create(
+        id=faker.uuid4(),
+        tank=tank,
+        volume=faker.pyfloat(positive=True, right_digits=2),
+        timestamp=faker.past_datetime(start_date=f"-{random.randint(1, 365)}d")
+    )
+    return tank
